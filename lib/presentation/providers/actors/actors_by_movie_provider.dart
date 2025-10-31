94451efc91d7e3e7877263a140bbd942c1f45e27
Gastygas <gastonrodrigogonzalez55@gmail.com>
2025-10-31 19:57:58 -0300

    add:getActors implementation

---
+++ lib/presentation/providers/actors/actors_by_movie_provider.dart
@@ -0,0 +1,25 @@
+import 'package:cinemapedia/domain/entities/actor.dart';
+import 'package:cinemapedia/presentation/providers/actors/actors_repository_provider.dart';
+import 'package:flutter_riverpod/legacy.dart';
+
+final actorsByMoviesProvider = StateNotifierProvider<ActorsByMovieNotifer, Map<String, List<Actor>>>((ref) {
+  final getActor = ref.watch(actorsRepositoryProvider).getActorsByMovie;
+  return ActorsByMovieNotifer(getActors: getActor);
+});
+
+typedef GetActorsCallback = Future<List<Actor>> Function(String movieId);
+
+class ActorsByMovieNotifer extends StateNotifier<Map<String, List<Actor>>> {
+  final GetActorsCallback getActors;
+
+  ActorsByMovieNotifer({required this.getActors}) : super({});
+
+  Future<void> loadActors(String movieId) async {
+    if (state[movieId] != null) return;
+    //No se va a hacer otra peticion de la misma movie si ya se hizo antes
+
+    final List<Actor> actors = await getActors(movieId);
+
+    state = {...state, movieId: actors};
+  }
+}
